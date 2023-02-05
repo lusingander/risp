f9f312fa12f2796123d6fe432cfcbbbc40cfbd61
Kyosuke Fujimoto <kyoro.f@gmail.com>
2023-02-05 10:08:24 +0900

    environment

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ enum RispExp {
     Symbol(String),
     Number(f64),
     List(Vec<RispExp>),
+    Func(fn(&[RispExp]) -> Result<RispExp, RispErr>),
 }
 
 enum RispErr {
@@ -58,5 +59,41 @@ fn parse_atom(token: &str) -> RispExp {
     match potential_float {
         Ok(v) => RispExp::Number(v),
         Err(_) => RispExp::Symbol(token.to_string().clone()),
+    }
+}
+
+fn default_env() -> RispEnv {
+    let mut data: HashMap<String, RispExp> = HashMap::new();
+    data.insert(
+        "+".to_string(),
+        RispExp::Func(|args| {
+            let sum = parse_list_of_floats(args)?
+                .iter()
+                .fold(0.0, |sum, a| sum + a);
+            Ok(RispExp::Number(sum))
+        }),
+    );
+    data.insert(
+        "-".to_string(),
+        RispExp::Func(|args| {
+            let floats = parse_list_of_floats(args)?;
+            let (first, rest) = floats
+                .split_first()
+                .ok_or(RispErr::Reason("expected at least one number".to_string()))?;
+            let sum_of_rest = rest.iter().fold(0.0, |sum, a| sum + a);
+            Ok(RispExp::Number(first - sum_of_rest))
+        }),
+    );
+    RispEnv { data }
+}
+
+fn parse_list_of_floats(args: &[RispExp]) -> Result<Vec<f64>, RispErr> {
+    args.iter().map(parse_single_float).collect()
+}
+
+fn parse_single_float(exp: &RispExp) -> Result<f64, RispErr> {
+    match exp {
+        RispExp::Number(v) => Ok(*v),
+        _ => Err(RispErr::Reason("expected a number".to_string())),
     }
 }
